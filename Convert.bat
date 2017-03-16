9d899259478d573c7e335b86d4b3c49f738f0a34
Yi-Ju Tseng <beergt@gmail.com>
2017-03-16 19:12:32 +0800

    cvskit batch

---
+++ Convert.bat
@@ -0,0 +1,178 @@
+cd E:\BreastCancer
+
+if not exist 急診醫囑藥囑檔_1.csv (
+    in2csv 11109_急診醫囑藥囑檔_1.xlsx > 急診醫囑藥囑檔_1.csv
+)
+if not exist 疾病分類統計檔_1.csv (
+    in2csv 11109_疾病分類統計檔_1.xlsx > 疾病分類統計檔_1.csv
+)
+if not exist 解剖病理報告檔_1.csv (
+    in2csv 11109_解剖病理報告檔_1.xlsx > 解剖病理報告檔_1.csv
+)
+if not exist 檢驗結果歷史檔_1.csv (
+    in2csv 11109_檢驗結果歷史檔_1.xlsx > 檢驗結果歷史檔_1.csv
+)
+if not exist 檢驗結果歷史檔_2.csv (
+    in2csv 11109_檢驗結果歷史檔_2.xlsx > 檢驗結果歷史檔_2.csv
+)
+if not exist 檢驗結果歷史檔_2.csv (
+    in2csv 11109_檢驗結果歷史檔_2.xlsx > 檢驗結果歷史檔_2.csv
+)
+if not exist 檢驗結果歷史檔_3.csv (
+    in2csv 11109_檢驗結果歷史檔_3.xlsx > 檢驗結果歷史檔_3.csv
+)
+if not exist 檢驗結果歷史檔_4.csv (
+    in2csv 11109_檢驗結果歷史檔_4.xlsx > 檢驗結果歷史檔_4.csv
+)
+if not exist 檢驗結果歷史檔_5.csv (
+    in2csv 11109_檢驗結果歷史檔_5.xlsx > 檢驗結果歷史檔_5.csv
+)
+if not exist 檢驗結果歷史檔_6.csv (
+    in2csv 11109_檢驗結果歷史檔_6.xlsx > 檢驗結果歷史檔_6.csv
+)
+if not exist 檢驗結果歷史檔_7.csv (
+    in2csv 11109_檢驗結果歷史檔_7.xlsx > 檢驗結果歷史檔_7.csv
+)
+if not exist 檢驗結果歷史檔_8.csv (
+    in2csv 11109_檢驗結果歷史檔_8.xlsx > 檢驗結果歷史檔_8.csv
+)
+if not exist 檢驗結果歷史檔_9.csv (
+    in2csv 11109_檢驗結果歷史檔_9.xlsx > 檢驗結果歷史檔_9.csv
+)
+if not exist 檢驗結果歷史檔_10.csv (
+    in2csv 11109_檢驗結果歷史檔_10.xlsx > 檢驗結果歷史檔_10.csv
+)
+if not exist 檢驗結果歷史檔_11.csv (
+    in2csv 11109_檢驗結果歷史檔_11.xlsx > 檢驗結果歷史檔_11.csv
+)
+if not exist 檢驗結果歷史檔_12.csv (
+    in2csv 11109_檢驗結果歷史檔_12.xlsx > 檢驗結果歷史檔_12.csv
+)
+if not exist 檢驗結果歷史檔_13.csv (
+    in2csv 11109_檢驗結果歷史檔_13.xlsx > 檢驗結果歷史檔_13.csv
+)
+if not exist 住診醫囑檔_1.csv (
+    in2csv 11109_住診醫囑檔_1.xlsx > 住診醫囑檔_1.csv
+)
+if not exist 住診醫囑檔_2.csv (
+    in2csv 11109_住診醫囑檔_2.xlsx > 住診醫囑檔_2.csv
+)
+if not exist 住診醫囑檔_3.csv (
+    in2csv 11109_住診醫囑檔_3.xlsx > 住診醫囑檔_3.csv
+)
+if not exist 住診醫囑檔_4.csv (
+    in2csv 11109_住診醫囑檔_4.xlsx > 住診醫囑檔_4.csv
+)
+if not exist 住診醫囑檔_5.csv (
+    in2csv 11109_住診醫囑檔_5.xlsx > 住診醫囑檔_5.csv
+)
+if not exist 住診藥囑檔_1.csv (
+    in2csv 11109_住診藥囑檔_1.xlsx > 住診藥囑檔_1.csv
+)
+if not exist  (
+    in2csv 11109_住診藥囑檔_2.xlsx > 住診藥囑檔_2.csv
+)
+if not exist 住診藥囑檔_3.csv (
+    in2csv 11109_住診藥囑檔_3.xlsx > 住診藥囑檔_3.csv
+)
+if not exist 門診診斷檔_1.csv (
+    in2csv 11109_門診診斷檔_1.xlsx > 門診診斷檔_1.csv
+)
+if not exist 門診診斷檔_2.csv (
+    in2csv 11109_門診診斷檔_2.xlsx > 門診診斷檔_2.csv
+)
+if not exist 門診診斷檔_3.csv (
+    in2csv 11109_門診診斷檔_3.xlsx > 門診診斷檔_3.csv
+)
+if not exist 門診診斷檔_4.csv (
+    in2csv 11109_門診診斷檔_4.xlsx > 門診診斷檔_4.csv
+)
+if not exist 門診診斷檔_5.csv (
+    in2csv 11109_門診診斷檔_5.xlsx > 門診診斷檔_5.csv
+)
+if not exist 門診診斷檔_6.csv (
+    in2csv 11109_門診診斷檔_6.xlsx > 門診診斷檔_6.csv
+)
+if not exist 門診診斷檔_7.csv (
+    in2csv 11109_門診診斷檔_7.xlsx > 門診診斷檔_7.csv
+)
+if not exist 門診醫囑基本資料檔_1.csv (
+    in2csv 11109_門診醫囑基本資料檔_1.xlsx > 門診醫囑基本資料檔_1.csv
+)
+if not exist 門診醫囑基本資料檔_2.csv (
+    in2csv 11109_門診醫囑基本資料檔_2.xlsx > 門診醫囑基本資料檔_2.csv
+)
+if not exist 門診醫囑基本資料檔_3.csv (
+    in2csv 11109_門診醫囑基本資料檔_3.xlsx > 門診醫囑基本資料檔_3.csv
+)
+if not exist 門診醫囑藥囑主檔_1.csv (
+    in2csv 11109_門診醫囑藥囑主檔_1.xlsx > 門診醫囑藥囑主檔_1.csv
+)
+if not exist 門診醫囑藥囑主檔_2.csv (
+    in2csv 11109_門診醫囑藥囑主檔_2.xlsx > 門診醫囑藥囑主檔_2.csv
+)
+if not exist 門診醫囑藥囑主檔_3.csv (
+    in2csv 11109_門診醫囑藥囑主檔_3.xlsx > 門診醫囑藥囑主檔_3.csv
+)
+if not exist 門診醫囑藥囑主檔_4.csv (
+    in2csv 11109_門診醫囑藥囑主檔_4.xlsx > 門診醫囑藥囑主檔_4.csv
+)
+if not exist 門診醫囑藥囑主檔_5.csv (
+    in2csv 11109_門診醫囑藥囑主檔_5.xlsx > 門診醫囑藥囑主檔_5.csv
+)
+if not exist 門診醫囑藥囑主檔_6.csv (
+    in2csv 11109_門診醫囑藥囑主檔_6.xlsx > 門診醫囑藥囑主檔_6.csv
+)
+if not exist 門診醫囑藥囑主檔_7.csv (
+    in2csv 11109_門診醫囑藥囑主檔_7.xlsx > 門診醫囑藥囑主檔_7.csv
+)
+if not exist 門診醫囑藥囑主檔_8.csv (
+    in2csv 11109_門診醫囑藥囑主檔_8.xlsx > 門診醫囑藥囑主檔_8.csv
+)
+if not exist 門診醫囑藥囑主檔_9.csv (
+    in2csv 11109_門診醫囑藥囑主檔_9.xlsx > 門診醫囑藥囑主檔_9.csv
+)
+if not exist 門診醫囑藥囑主檔_10.csv (
+    in2csv 11109_門診醫囑藥囑主檔_10.xlsx > 門診醫囑藥囑主檔_10.csv
+)
+if not exist 急診診斷檔_1.csv (
+    in2csv 11109_急診診斷檔_1.xlsx > 急診診斷檔_1.csv
+)
+if not exist 急診醫囑基本資料檔_1.csv (
+    in2csv 11109_急診醫囑基本資料檔_1.xlsx > 急診醫囑基本資料檔_1.csv
+)
+if not exist 急診醫囑檔_1.csv (
+    in2csv 11109_急診醫囑檔_1.xlsx > 急診醫囑檔_1.csv
+)
+if not exist 急診醫囑檔_2.csv (
+    in2csv 11109_急診醫囑檔_2.xlsx > 急診醫囑檔_2.csv
+)
+if not exist 癌症登記治療檔_1.csv (
+    in2csv 11109_癌症登記治療檔_1.xlsx > 癌症登記治療檔_1.csv
+)
+if not exist 癌症登記診斷檔_1.csv (
+    in2csv 11109_癌症登記診斷檔_1.xlsx > 癌症登記診斷檔_1.csv
+)
+if not exist 歸戶主檔_1.csv (
+    in2csv 11109_歸戶主檔_1.xlsx > 歸戶主檔_1.csv
+)
+if not exist 門診醫囑醫囑主檔_1.csv (
+    in2csv 11109_門診醫囑醫囑主檔_1.xlsx > 門診醫囑醫囑主檔_1.csv
+)
+if not exist 門診醫囑醫囑主檔_2.csv (
+    in2csv 11109_門診醫囑醫囑主檔_2.xlsx > 門診醫囑醫囑主檔_2.csv
+)
+if not exist 門診醫囑醫囑主檔_3.csv (
+    in2csv 11109_門診醫囑醫囑主檔_3.xlsx > 門診醫囑醫囑主檔_3.csv
+)
+if not exist 門診醫囑醫囑主檔_4.csv (
+    in2csv 11109_門診醫囑醫囑主檔_4.xlsx > 門診醫囑醫囑主檔_4.csv
+)
+if not exist 門診醫囑醫囑主檔_5.csv (
+    in2csv 11109_門診醫囑醫囑主檔_5.xlsx > 門診醫囑醫囑主檔_5.csv
+)
+
+
+pause
+
+
