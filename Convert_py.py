9fb43fc780410c5e0b18d0f3c1231bc912147cba
Yi-Ju Tseng <beergt@gmail.com>
2024-07-31 10:23:36 +0800

    Add files via upload
    xlsx to csv for CGRD

---
+++ Convert_py.py
@@ -0,0 +1,23 @@
+
+#importing pandas as pd 
+import pandas as pd 
+import os
+import re
+
+path = "D:/DM/"
+files = os.listdir(path)
+
+files_xlsx = [f for f in files if f[-4:] == 'xlsx']
+
+for f in files_xlsx:
+    csvf = re.sub("xlsx","csv",f)
+    if(os.path.isfile(csvf)):
+        print(csvf+" file exist")
+        next
+    print("old file: "+f+", new file: "+csvf)
+
+    read_file = pd.read_excel (f) 
+    read_file.to_csv (csvf,  
+                    index = None, 
+                    header=True) 
+    
